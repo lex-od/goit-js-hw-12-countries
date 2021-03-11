2a222f83b38a6bd029bf0e98d383f4e9e99c1811
Aleksey Yesin <esinru@gmail.com>
2021-03-11 20:56:28 +0200

    PNotify added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './styles.css';
 import fetchCountries from './js/fetchCountries';
 import debounce from 'lodash.debounce';
+import { error, defaults as pnDefaults } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+pnDefaults.delay = 2000;
 
 const MAX_DISPLAY_COUNTRIES = 10;
 
@@ -11,23 +16,22 @@ const refs = {
 refs.countryInput.addEventListener('input', debounce(onCounryInput, 500));
 
 function onCounryInput(e) {
-  const country = e.target.value;
-  if (!country) {
-    return;
-  }
-
-  fetchCountries(country)
-    .then(countries => {
-      if (countries.length > MAX_DISPLAY_COUNTRIES) {
-        //
-        console.log('c > 10');
-      } else if (countries.length > 1) {
-        //
-        console.log('c > 1');
-      } else if ((countries.length = 1)) {
-        //
-        console.log('c = 1');
-      }
-    })
-    .catch(console.error);
+  // const country = e.target.value;
+  // if (!country) {
+  //   return;
+  // }
+  // fetchCountries(country)
+  //   .then(countries => {
+  //     if (countries.length > MAX_DISPLAY_COUNTRIES) {
+  //       //
+  //       console.log('c > 10');
+  //     } else if (countries.length > 1) {
+  //       //
+  //       console.log('c > 1');
+  //     } else if ((countries.length = 1)) {
+  //       //
+  //       console.log('c = 1');
+  //     }
+  //   })
+  //   .catch(console.error);
 }
