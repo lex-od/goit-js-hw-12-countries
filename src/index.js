8ede8dfc624e32918ab1925d9f7d41dfffa1fe90
Aleksey Yesin <esinru@gmail.com>
2021-03-11 21:57:30 +0200

    Country list added

--- src/index.js
+++ src/index.js
@@ -4,34 +4,43 @@ import debounce from 'lodash.debounce';
 import { error, defaults as pnDefaults } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
+import countryListTmpl from './data/country-list.hbs';
 
-pnDefaults.delay = 2000;
+pnDefaults.delay = 3000;
 
 const MAX_DISPLAY_COUNTRIES = 10;
 
 const refs = {
   countryInput: document.querySelector('#country-input'),
+  countryListWr: document.querySelector('#country-list-wrapper'),
+  countryList: document.querySelector('#country-list-wrapper > ul'),
+  countryCardWr: document.querySelector('#country-card-wrapper'),
 };
 
 refs.countryInput.addEventListener('input', debounce(onCounryInput, 500));
 
 function onCounryInput(e) {
-  // const country = e.target.value;
-  // if (!country) {
-  //   return;
-  // }
-  // fetchCountries(country)
-  //   .then(countries => {
-  //     if (countries.length > MAX_DISPLAY_COUNTRIES) {
-  //       //
-  //       console.log('c > 10');
-  //     } else if (countries.length > 1) {
-  //       //
-  //       console.log('c > 1');
-  //     } else if ((countries.length = 1)) {
-  //       //
-  //       console.log('c = 1');
-  //     }
-  //   })
-  //   .catch(console.error);
+  const country = e.target.value;
+  if (!country) {
+    return;
+  }
+  fetchCountries(country)
+    .then(countries => {
+      if (countries.length > MAX_DISPLAY_COUNTRIES) {
+        error(
+          'Слишком большое количество результатов. Пожалуйста, введите более специфичный запрос!',
+        );
+
+        console.log('c > 10');
+      } else if (countries.length > 1) {
+        refs.countryList.innerHTML = countryListTmpl(countries);
+
+        console.log('c > 1');
+      } else if ((countries.length = 1)) {
+        //
+
+        console.log('c = 1');
+      }
+    })
+    .catch(console.error);
 }
